55f3ae7e3ef99986c3a246bbd356a2aea08c1237
Iceman <chenzq712@163.com>
2018-01-22 02:02:55 +0800

    add paging utils

--- src/main/java/com/equestrianism/api/core/utils/PageUtils.java
+++ src/main/java/com/equestrianism/api/core/utils/PageUtils.java
@@ -5,4 +5,19 @@ package com.equestrianism.api.core.utils;
  */
 public class PageUtils {
 
+    public static Integer calculateTotalPages( Integer totalRecorders, Integer pageRecorders ) {
+        if ( totalRecorders <= 0 || pageRecorders <= 0  ) {
+            return 0;
+        }
+        if ( totalRecorders % pageRecorders == 0 ) {
+            return totalRecorders / pageRecorders;
+        }
+        return totalRecorders / pageRecorders + 1;
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( 4 / 3 );
+        System.out.println( calculateTotalPages( 13, 10 ) );
+    }
+
 }
